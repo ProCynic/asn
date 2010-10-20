552a3537e5feb8377755f7b42c61174e54d30eb4
Geofrey Parker <professional.cynic@gmail.com>
2010-10-20 16:27:33 -0500

    Filling out the class defs.

--- datastore.py
+++ datastore.py
@@ -3,18 +3,35 @@
 
 import re
 from google.appengine.ext import db
+from db import polymodel
 from google.appengine.api import users
 
-class ratable(db.model):
-    #member vars
+class ratable(polymodel.PolyModel):
     pass
+
+class rating(db.model):
+    rating = db.Rating(required=True)
+    comment = db.Text()
+    rated = db.ReferenceProperty(ratable)
 
 class course (ratable):
     unique = db.StringProperty(required=True,validator=uniqueValidator)
     courseNum = db.StringProperty(required=True)
     name = db.StringProperty(required=True)
-    semeseter = db.StringProperty(required=True,choices=['FALL','SPRING','SUMMER'])
-    
+    semester = db.StringProperty(required=True,choices=['FALL','SPRING','SUMMER'])
+    instructor = db.StringProperty(required=True)
+    grade = db.StringProperty(required=True)
+    year = db.StringProperty(requierd=True,validator=(lambda x: raise ValueError if re.search("^\d{4}$",x) is None))
+
+class book(ratable):
+    isbn = db.StringProperty(required=True)
+    title = db.StringProperty()
+    author = db.StringProperty()
+
+class paper(ratable):
+    journal = db.StringProperty()
+    title = db.StringProperty()
+    author = db.StringProperty()
 
 
 class comment(db.model):
