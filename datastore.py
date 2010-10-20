38fd2ff0abc56b4183862d6ad0d3804db86e6bfa
Geofrey Parker <professional.cynic@gmail.com>
2010-10-20 17:16:27 -0500

    Filling out validators

--- datastore.py
+++ datastore.py
@@ -2,20 +2,26 @@
 """
 
 from google.appengine.ext import db
-from db import polymodel
+from google.appengine.ext.db import polymodel
 from google.appengine.api import users
 from validators import *
+
+class comment(db.Model):
+    """
+    """
+    text = db.TextProperty()
+    replyto = SelfReferenceProperty()
 
 class ratable(polymodel.PolyModel):
     """
     """
     pass
 
-class rating(db.model):
+class rating(db.Model):
     """
     """
-    rating = db.Rating(required=True)
-    comment = db.Text()
+    rating = db.RatingProperty(required=True)
+    comment = db.ReferenceProperty(comment)
     rated = db.ReferenceProperty(ratable)
 
 class course (ratable):
@@ -25,32 +31,34 @@ class course (ratable):
     courseNum = db.StringProperty(required=True, validator=courseNumValidator)
     name = db.StringProperty(required=True)
     semester = db.StringProperty(required=True,choices=['FALL','SPRING','SUMMER'])
-    instructor = db.StringProperty(required=True)
-    grade = db.StringProperty(required=True)
-    year = db.StringProperty(requierd=True)
+    instructor = db.StringProperty(required=True, validator=personNameValidator)
+    grade = db.StringProperty(required=True, validator=gradeValidator)
+    year = db.StringProperty(requierd=True, validator=yearValidator)
 
 
 
 class book(ratable):
     """
     """
-    isbn = db.StringProperty(required=True)
+    isbn = db.StringProperty(validator=isbnValidator)
     title = db.StringProperty()
-    author = db.StringProperty()
+    author = db.StringProperty(validator=personNameValidator)
 
 class paper(ratable):
     """
     """
     journal = db.StringProperty()
     title = db.StringProperty()
-    author = db.StringProperty()
+    author = db.StringProperty(validator=personNameValidator)
 
 
 class place (ratable):
     """
     """
     location = db.StringProperty(required=True)
-    semeseter = db.StringProperty(required=True,choices=['FALL','SPRING','SUMMER'])
+    semester = db.StringProperty(required=True,choices=['FALL','SPRING','SUMMER'])
+    year = db.StringProperty(validator=yearValidator)
+    latLong = db.GeoPtProperty()
 
 class internship (place):
     """
@@ -78,7 +86,23 @@ class game (ratable):
     platform = db.StringProperty(required=True)
     title = db.StringProperty(required=True)
 
-class comment(db.model):
-    """
-    """
-    text = db.Text()
+gamePlatforms = [
+    "XBOX",
+    "XBOX 360",
+    "PLAYSTATION",
+    "PLAYSTATION 2",
+    "PLAYSTATION 3",
+    "NINTINDO",
+    "SUPER NINTINDO",
+    "NINTINDO 64",
+    "GAMECUBE",
+    "WII",
+    "PC",
+    "MAC",
+    "LINUX",
+    "GAMEBOY",
+    "NINTINDO DS",
+    "PSP",
+    "IPHONE",
+    "ANDROID",
+    "CELLPHONE"]
--- validators.py
+++ validators.py
@@ -2,8 +2,41 @@
 """
 import re
 
+#Template
+def Validator(value):
+    """
+    """
+    pattern = "^$"
+    if re.search(pattern,value) is None: raise ValueError
+
 def uniqueValidator(value):
     """
     """
-    unique = re.search("^\d{5}$",value)
-    if unique is None: raise ValueError
+    pat
+    if re.search("^\d{5}$",value) is None: raise ValueError
+
+
+def courseNumValidator(value):
+    """
+    """
+    pattern = "^[A-Z]{1,3}\s\d{3}[a-zA-Z]?$"
+    if re.search(pattern,value) is None: raise ValueError
+
+
+def personNameValidator(value):
+    """
+    """
+    pattern = "^.*$"
+    if re.search(pattern,value) is None: raise ValueError
+
+def gradeValidator(value):
+    """
+    """
+    pattern = "^([B-D][\+|\-]?)|A\-?|F$"
+    if re.search(pattern,value) is None: raise ValueError
+
+def yearValidator(value):
+    """
+    """
+    pattern = "^\d{4}$"
+    if re.search(pattern,value) is None: raise ValueError
