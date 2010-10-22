861ffe1e9f258ee0c757f4910c3ff4e726e61353
Geofrey Parker <professional.cynic@gmail.com>
2010-10-22 14:09:45 -0500

    filling out accessors

--- dataAccessors.py
+++ dataAccessors.py
@@ -4,33 +4,50 @@
 import re
 from datastore import *
 
-class datastoreController:
+def _addPerson(fname,lname, mname=None):
     """
     """
-    __init__(self):
-        pass
+    #if person in datastore:
+        #return person.key()
+    p = Person(fname=fname,
+               lname=lname,
+               mname=mname)
+    p.put()
+    return p.key()
 
-    def addPerson(fname,lname, mname=None):
-        #if person in datastore:
-            #return person.key()
-        p = Person(fname=fname,
-                   lname=lname,
-                   mname=mname)
-        p.put()
-        return p.key()
+def addBook(title,isbn,author):
+    """
+    """
+    isbn = isbn.strip().replace('-','')
+    #pull out fname, lname, mname from author
 
-    def addBook(title,isbn,author):
-        isbn = isbn.strip().replace('-','')
-        #pull out fname, lname, mname from author
+    a = _addPerson(afname,alname,amname)
+    b = Book(title=title,
+             isbn=isbn,
+             author=a)
 
-        a = _addPerson(afname,alname,amname)
-        b = Book(title=title,
-                 isbn=isbn,
-                 author=a)
+def addCourse(unique, courseNum, name, semester, instructor, grade):
+    """
+    """
+    #regexps to pull out data
+    i = _addPerson(fname, lname, mname)
+    c = Course(unique=unique,
+               courseNum=courseNum,
+               name=name,
+               semester=semester,
+               year=year,
+               grade=grade,
+               instructor=i)
+    c.put()
+    return c.key()
 
-    def addCourse(unique, courseNum, name, semester, instructor, grade):
-        #regexps to pull out data
-        i = _addPerson(fname, lname, mname)
-        
-    def addRating(rating, ratable, comment=None):
-        r = 
+    
+def addRating(ratable, student, rating, comment=None):
+    """
+    """
+    r = Rating(rated=ratable,
+               rater=student,
+               rating=rating,
+               comment=comment)
+    r.put()
+    return r.key()
