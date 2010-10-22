1cdaf6e2db3fa77f983ce46eab437213634bd817
Geofrey Parker <professional.cynic@gmail.com>
2010-10-22 13:53:49 -0500

    still writing accessors

--- dataAccessors.py
+++ dataAccessors.py
@@ -1,21 +1,36 @@
-addBook(title,isbn,author):
-    isbn = isbn.strip().replace('-','')
-    #pull out fname, lname, mname from author
+"""
+"""
 
-    a = addPerson(afname,alname,amname)
-    b = Book(title=title,
-             isbn=isbn,
-             author=a)
+import re
+from datastore import *
 
-def addPerson(fname,lname, mname=None):
-    #if person in datastore:
-        #return person.key()
-    p = Person(fname=fname,
-               lname=lname,
-               mname=mname)
-    p.put()
-    return p.key()
+class datastoreController:
+    """
+    """
+    __init__(self):
+        pass
 
-def addCourse(unique, courseNum, name, semester, instructor, grade):
-    #regexps to pull out data
-    
+    def addPerson(fname,lname, mname=None):
+        #if person in datastore:
+            #return person.key()
+        p = Person(fname=fname,
+                   lname=lname,
+                   mname=mname)
+        p.put()
+        return p.key()
+
+    def addBook(title,isbn,author):
+        isbn = isbn.strip().replace('-','')
+        #pull out fname, lname, mname from author
+
+        a = _addPerson(afname,alname,amname)
+        b = Book(title=title,
+                 isbn=isbn,
+                 author=a)
+
+    def addCourse(unique, courseNum, name, semester, instructor, grade):
+        #regexps to pull out data
+        i = _addPerson(fname, lname, mname)
+        
+    def addRating(rating, ratable, comment=None):
+        r = 
