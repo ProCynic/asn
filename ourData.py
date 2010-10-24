8e70c94741cd5cdf5d8bbfa9f19e95cdf121ae0d
Albert Wang <ayw69@cs.utexas.edu>
2010-10-24 15:17:19 -0500

    Adding my own data. importds.py is a proxy for dataaccessors, to make unittesting the importer not reliant on the datastore itself.

--- ourData.py
+++ ourData.py
@@ -41,6 +41,31 @@ def aywang():
     pwd = passgen()
     me = addStudent(sid,pwd)
 
+	b = addBook("Dune, 40th Anniversary Edition (Dune Chronicles, Book 1)", "0441013597", "Frank Herbert")
+	addRating(b, me, '90', "Great book, but the sequels are kinda poor.")
+
+	b = addCourse("52540", "C S 375", "Software Engineering", "Fall", "2010", "Glen Downing")
+	addRating(b, me, '75', "Fun assignments, but the class itself bores me.")
+	addGrade(b, me, 'A')
+	
+	b = addPaper("conference", "Improved Alpha-Tested Magnification for Vector Textures and Special Effects", "Chris Green")
+	addRating(b, me, '100', "Great paper that explains how Valve used the GPU to render text clearly.")
+
+	b = addGame("Xbox360", "Blazblue: Continumn Shift")
+	addRating(b, me, '80', "Great fighter game, sometimes lags online.")
+
+	b = addPlaceLive("Duval Villas", "4305 Duval St." "Fall", "2009")
+	addRating(b, me, '30', "A bit expensive for what you get.")
+
+	b = addPlaceEat('The Ironworks', '100 Red River St.', "Fall", "2010")
+	addRating(b, me, '100', "I love eating here, the beef ribs are delicious.")
+
+	b = addPlaceFun('Arcade UFO', '3101 Speedway', "Fall", "2010")
+	addRating(b, me, '80', "Great arcade place in Austin, one of the few left.")
+
+	b = addPlaceStudy('ENS Labs', 'ENS Basement', "Fall", "2010")
+	addRating(b, me, '86', "No taylor basement, but its still filled with intelligent people to help.")
+
 def ttb265():
     sid = sidgen()
     pwd = passgen()
