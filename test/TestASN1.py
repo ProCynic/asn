af0684cbb573eaaaf21cdb762fd7d8b34b6b9751
Tucker Bickler <Tucker@wireless-128-62-157-84.public.utexas.edu>
2010-10-24 19:47:02 -0500

    added more dataAccessors tests

--- test/TestASN1.py
+++ test/TestASN1.py
@@ -2,9 +2,25 @@ import unittest
 from validators import *
 from dataAccessors import *
 from google.appengine.ext import db
+import random
+
+def randString(n):
+	alphanum = "abcdefghijklmnopqrstuvwxyz"
+	alphanum += alphanum.upper()
+	alphanum += "0123456789"
+	alphanum += "~!@#$%^&*()-_=+:;/?"
+	string = ""
+	gen = random.Random()
+	for x in range(n):
+		string += alphanum[gen.randint(0,len(alphanum)-1)]
+	return string
+def sidgen():
+	return randString(8)
+def passgen():
+	return randString(12)
 
 class TestASN1(unittest.TestCase):
-        
+
     # dataAccessors Tests
 
     def test_addGame(self):
@@ -27,15 +43,14 @@ class TestASN1(unittest.TestCase):
         self.assertTrue( db_i.year == '2010' )
         
     def test_addStudent(self):
-        #da = DataAccessor()
-        #sid = sidgen()
-        #pwd = passgen()
-        #s = da.addStudent(sid,pwd)
+        da = DataAccessor()
+        sid = sidgen()
+        pwd = passgen()
+        s = da.addStudent(sid,pwd)
         
-        #db_s = db.get(s)
-        #self.assertTrue( False ) """ validate sid """
-        #self.assertTrue( False ) """ validate password """
-        pass
+        db_s = db.get(s)
+        self.assertTrue( len(db_s.password) == 12 )
+        self.assertTrue( len(db_s.sid) == 8 )
         
     def test_addPaper(self):
         da = DataAccessor()
@@ -49,10 +64,16 @@ class TestASN1(unittest.TestCase):
     
     def test_addRating(self):
         da = DataAccessor()
+        sid = sidgen()
+        pwd = passgen()
+        s = da.addStudent(sid,pwd)
         p = da.addPaper("conference", "Improved Alpha-Tested Magnification for Vector Textures and Special Effects", "Chris Green")
-        r = da.addRating(p, me, '100', "Great paper that explains how Valve used the GPU to render text clearly.")
+        r = da.addRating(p, s, '100', "Great paper that explains how Valve used the GPU to render text clearly.")
         
+        db_s = db.get(s)
+        db_p = db.get(p)
         db_r = db.get(r)
+        self.assertTrue( db_r.rater is db_s )
         
         # not complete
         
