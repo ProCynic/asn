5e69e171a95aaa53d97d64e51ef1218fcc5b2941
Matt Wanninger <mwanninger@gmail.com>
2010-10-24 19:55:19 -0500

    Made fixes to validators.py, added unit test to TestValidators.py

---
+++ test/TestValidators.py
@@ -0,0 +1,134 @@
+import unittest
+from validators import *
+
+class TestValidators(unittest.TestCase):
+    """
+    """
+    # ---------------
+    # uniqueValidator
+    # ---------------
+        
+    def test_uniqueValidator_1(self):
+    	try:
+    	    uniqueValidator("43213")
+    	except ValueError:
+    		self.assertTrue(False)
+    
+    def test_uniqueValidator_2(self):
+    	self.assertRaises(ValueError, uniqueValidator, "432133")
+    		
+    def test_uniqueValidator_3(self):
+    	self.assertRaises(ValueError, uniqueValidator, "1111")
+    		
+    def test_uniqueValidator_4(self):
+    	self.assertRaises(ValueError, uniqueValidator, "abcde")
+    		
+    # ------------------
+    # courseNumValidator
+    # ------------------
+    
+    def test_courseNumValidator_1(self):
+    	try:
+    	    courseNumValidator("CS 373")
+    	except ValueError:
+    		self.assertTrue(False)
+    
+    def test_courseNumValidator_2(self):
+    	try:
+    	    courseNumValidator("CS 373a")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_courseNumValidator_3(self):
+    	self.assertRaises(ValueError, courseNumValidator, "CS 373aa")
+    		
+    def test_courseNumValidator_4(self):
+    	self.assertRaises(ValueError, courseNumValidator, "CS373")
+    		
+    def test_courseNumValidator_5(self):
+    	try:
+    	    courseNumValidator("CSS 373")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_courseNumValidator_6(self):
+    	self.assertRaises(ValueError, courseNumValidator, "CS 37a")
+    		
+    def test_courseNumValidator_7(self):
+    	self.assertRaises(ValueError, courseNumValidator, " 373")
+    		
+    # --------------
+    # gradeValidator
+    # --------------
+    
+    def test_gradeValidator_1(self):
+        self.assertRaises(ValueError, gradeValidator, "")
+    		
+    def test_gradeValidator_2(self):
+       self.assertRaises(ValueError, gradeValidator, "A+")
+    		
+    def test_gradeValidator_3(self):
+        try:
+    	    gradeValidator("A")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_gradeValidator_4(self):
+        try:
+    	    gradeValidator("B+")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_gradeValidator_5(self):
+        try:
+    	    gradeValidator("C-")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_gradeValidator_6(self):
+        self.assertRaises(ValueError, gradeValidator, "C--")
+        
+    def test_gradeValidator_7(self):
+        self.assertRaises(ValueError, gradeValidator, "D++")
+        
+    def test_gradeValidator_8(self):
+        self.assertRaises(ValueError, gradeValidator, "AA")
+        
+    # -------------
+    # yearValidator
+    # -------------
+    
+    def test_yearValidator_1(self):
+        try:
+    	    yearValidator("1974")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_yearValidator_2(self):
+        self.assertRaises(ValueError, yearValidator, "198A")
+       
+    def test_yearValidator_3(self):
+        self.assertRaises(ValueError, yearValidator, "19873")
+        
+    def test_yearValidator_4(self):
+        self.assertRaises(ValueError, yearValidator, "198")
+        
+    # -------------
+    # isbnValidator
+    # -------------
+    
+    def test_isbnValidator_1(self):
+        try:
+    	    isbnValidator("1594202664")
+    	except ValueError:
+    		self.assertTrue(False)
+    		
+    def test_isbnValidator_2(self):
+        try:
+    	    isbnValidator("9781594202667")
+    	except ValueError:
+    		self.assertTrue(False)
+   
+
+if __name__ == '__main__' :
+	unittest.main()
--- validators.py
+++ validators.py
@@ -26,9 +26,9 @@ def courseNumValidator(value):
 def gradeValidator(value):
     """
     """
-    pattern = "^([B-D][\+|\-]?)|(A\-?)|F$"
-    if re.search(pattern,value) is None: raise ValueError
-
+    #pattern = "^([B-D][\+|\-]?)|(A\-?)|F$"
+    #if re.search(pattern,value) is None: raise ValueError
+    if value not in ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'] : raise ValueError
 def yearValidator(value):
     """
     """
@@ -41,11 +41,11 @@ def isbnValidator(value):
     pattern = "^\d{10}|\d{13}$"
     if re.search(pattern,value) is None: raise ValueError
     x = [int(i) for i in value[:-1]]
-    s = value[-1]
+    s = int(value[-1])
     #For ISBN-10
     if len(value) == 10:
-        if s != sum(x[i]*(10-i) for i in range(len(x))) % 11: raise ValueError
+        if s != 11 - sum(x[i]*(10-i) for i in range(len(x))) % 11: raise ValueError
     #For ISBN-13
-    for i in range(1,len(x),2):
+    for i in range(1,len(x)+1,2):
         x[i] *= 3
     if s != 10 - (sum(x) % 10): raise ValueError
