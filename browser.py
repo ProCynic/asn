b02cd00deebd3291d9a1e0fd36eb03d72fbb6d5a
Albert Wang <ayw69@cs.utexas.edu>
2010-11-05 15:04:43 -0500

    Adding a bit more functionality to the browser

--- browser.py
+++ browser.py
@@ -9,21 +9,58 @@ from importer import Importer
 from dataAccessors import DataAccessor, addTypename
 import dataStore as DS
 
-
-
-
 class Browser(BaseRequestHandler) :
 
-    def get(self) :
+    def get(self, arg = None, sort = None) :
         """
             Create the browser home page.
         """
-        DA = DataAccessor()
 
-        query = prepareDataForTemplate(DS.Ratable.all())
+        possiblemap = {
+            "all" : DS.Ratable,
+            "courses" : DS.Course, 
+            "books" : DS.Book,
+            "papers" : DS.Paper,
+            "internships" : DS.Internship,
+            "livingplaces" : DS.PlaceLive, 
+            "eatingplaces" : DS.PlaceEat, 
+            "funplaces" : DS.PlaceFun,
+            "studyingplaces" : DS.PlaceStudy,
+            "games" : DS.Game
+        }
+
+        if arg : 
+            arg = arg.lower()
+
+        if sort :
+            sort = sort.lower()
+
+        query = None
+        if arg and arg in possiblemap :
+            query = prepareDataForTemplate(possiblemap[arg].all())
+        else :
+            query = prepareDataForTemplate(DS.Ratable.all())
+
+        if sort and sort in ['asc', 'desc'] :
+            if sort == 'asc' :
+                query = sorted(query, key= lambda x : x.rating)
+            elif sort == 'desc' :
+                query = sorted(query, key= lambda x : x.rating, reverse = True)
+
+        if arg and not arg in possiblemap :
+            setSessionMessage(getSessionByRequest(self), "That was an invalid query; showing all results instead.")
+
         
-        ratings = DA.getAllRatings()
         self.generate('browser.html', {
             'title' : "Home",
-            'ratings' : query 
+            'ratings' : query,
+            'arg' : arg,
+            'sort' : sort
         })
+
+    def post(self, unused = None, unused2 = None) :
+        filter = self.request.get('filter')
+        if not filter :
+            filter = 'all'
+
+        self.redirect('/browse/' + filter + "/" + self.request.get('sort'))
