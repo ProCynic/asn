202a17ccde0df09687c981a3030d162664e1029d
Albert Wang <ayw69@cs.utexas.edu>
2010-11-05 13:37:16 -0500

    Working on the browser view

--- browser.py
+++ browser.py
@@ -7,18 +7,59 @@ from importer import Importer
 from dataAccessors import DataAccessor
 import dataStore as DS
 
-class Browser(BaseRequestHandler) :
+
+def getAverageRating(item) :
+    query = DS.Rating.all()
+    query.filter('rated =', item)
+
+    items = 0
+    total = 0
+    for x in query : 
+        total += x.rating
+        items += 1
+
+    if items == 0 :
+        return 100
+
+    return (total / items, items);
+
+
+def getRatingClass(rating) :
+    if rating > 70 :
+        return 'good' 
     
+    if 70 >= rating > 40 :
+        return 'average'
+
+    return 'bad'
+
+
+class Browser(BaseRequestHandler) :
+
     def get(self) :
         """
             Create the browser home page.
         """
         DA = DataAccessor()
 
-        query = DS.Rating.all()
+        query = DS.Ratable.all()
+
+        temp = []
+        for x in query :
+            x.typename = x.__class__.__name__
+            x.rating, x.ratingCount = getAverageRating(x)
+            x.ratingClass = getRatingClass(x.rating)
+            temp.append(x)
+
+
+
+
+
+
+         
         
         ratings = DA.getAllRatings()
         self.generate('browser.html', {
             'title' : "Home",
-            'ratings' : query
+            'ratings' : temp
         })
