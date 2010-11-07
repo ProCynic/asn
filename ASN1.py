43efc825b62a924d55387da234308926618142b7
Albert Wang <ayw69@cs.utexas.edu>
2010-11-07 17:20:59 -0600

    More fixes

--- ASN1.py
+++ ASN1.py
@@ -115,7 +115,7 @@ class Ratable(BaseRequestHandler):
         """
         """
         ratable = db.get(db.Key(key))
-        unified = unify(ratable)
+        unified = prepareItem(ratable)
         
         ratings = DA.getAllRatings().filter('rated =',ratable)
        
