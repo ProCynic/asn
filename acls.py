fa6fb4ecd5002af4657b1c7579121d07ba590257
Geoffrey Parker <professional.cynic@gmail.com>
2010-11-07 19:39:18 -0600

    Fixed indentation

--- acls.py
+++ acls.py
@@ -8,36 +8,36 @@ from google.appengine.ext import db
 
 
 def __randomString(length) : 
-    characters =  "abcdefghijklmnopqrstuvwxyz"
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXZY"
-    characters += "0123456789"
-    characters += "~!@#$%^&*()_+-=:;/?"
+	characters =  "abcdefghijklmnopqrstuvwxyz"
+	characters += "ABCDEFGHIJKLMNOPQRSTUVWXZY"
+	characters += "0123456789"
+	characters += "~!@#$%^&*()_+-=:;/?"
 
-    generator = random.Random()
-    
-    string = ""
-    for x in range(length) :
-        string += characters[generator.randint(0, len(characters) - 1)];
-    return string
+	generator = random.Random()
+	
+	string = ""
+	for x in range(length) :
+		string += characters[generator.randint(0, len(characters) - 1)];
+	return string
 
 
 def userIDGen() : 
-    """
-        Returns a randomly generated UserID.
-    """
-    return __randomString(8)
+	"""
+		Returns a randomly generated UserID.
+	"""
+	return __randomString(8)
 
 def passwordGen() : 
-    """
-        Returns a randomly generated password.
-    """
-    return __randomString(12)
+	"""
+		Returns a randomly generated password.
+	"""
+	return __randomString(12)
 
 def admin(func) : 
     """
-        This function is used as a decorator on the pages.
-        Any user attempting to access admin protected pages will be 
-        redirected to the login page if they are not an admin.
+    This function is used as a decorator on the pages.
+    Any user attempting to access admin protected pages will be 
+    redirected to the login page if they are not an admin.
     """
     def redirectlogin(session, self) :
         setSessionMessage(session, "Admin Login Required.", True)
@@ -46,26 +46,26 @@ def admin(func) :
     def checkauth(*args, **kwargs) : 
         self = args[0]
 
-                   
+                           
         session = getSessionByRequest(self)
         user = getSessionUser(session)
 
         if not user :
-            return redirectlogin(session, self)
+                return redirectlogin(session, self)
 
         if user.userType == 'ADMIN' : 
-            return func(*args, **kwargs)
+                return func(*args, **kwargs)
 
         return redirectlogin(session, self)
     return checkauth
 
 def user(func) : 
-     """
-        This function is used as a decorator on the pages.
-        Any user attempting to access student protected pages will be 
-        redirected to the login page if they are not an student.
-     """
-   def redirectlogin(session, self) :
+    """
+    This function is used as a decorator on the pages.
+    Any user attempting to access student protected pages will be 
+    redirected to the login page if they are not an student.
+    """
+    def redirectlogin(session, self) :
         setSessionMessage(session, "Student Login required", True)
         return self.redirect('/login')
 
